a35dcb8dca296e4bfd9a04c797debb0c951c2c4b
jaime-espin <jaime.espinrodriguez@gmail.com>
2025-03-23 19:12:40 +0100

    resolución del problema automata de la sesion3

---
+++ sesion3/entrega/automata/src/ace.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "automata.h"
+
+using namespace std;
+
+int main() {
+    int regla, tmax;
+
+    cout << "Ingrese la regla (0-255): ";
+    cin >> regla;
+    cout << "Ingrese el número de iteraciones: ";
+    cin >> tmax;
+
+    if (regla < 0 || regla > 255 || tmax < 1) {
+        cout << "Error: Ingrese una regla entre 0-255 y al menos 1 iteración.\n";
+    }
+    
+    ejecutarAutomata(regla, tmax);
+}
