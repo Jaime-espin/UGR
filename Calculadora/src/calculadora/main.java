6c99c4bf066c6d50042cc182dcebfff9a26ca492
Jaime-espin <jaime.espinrodriguez@gmail.com>
2025-12-06 12:29:01 +0100

    Ejercicio de clase claculadora

---
+++ Calculadora/src/calculadora/main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Jaime Espín
+ */
+public class main {
+    public static void main(String args[]){
+        VentanaPrincipal v = new VentanaPrincipal();
+        
+        v.setVisible(true);
+    }
+    
+}
